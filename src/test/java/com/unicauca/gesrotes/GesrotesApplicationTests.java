6f6ccf8e3a3f9046dd8d9651aa3d7eb85384b75f
Jorge Andrés Zapata <57274200+acrip@users.noreply.github.com>
2023-03-28 10:48:46 -0500

    First commit

---
+++ src/test/java/com/unicauca/gesrotes/GesrotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicauca.gesrotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesrotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
